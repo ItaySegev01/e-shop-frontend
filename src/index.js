f511ff038a53b91ebbf1a0fd928901be0e7e1b7d
Itay Segev <itaysegev123@gmail.com>
2023-07-12 22:27:30 +0300

    .

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import './index.css';
 import {axios, React, ReactDOM, App, reportWebVitals, HelmetProvider, StoreProvider} from './Imports';
 
 
-axios.defaults.baseURL = 'http://localhost:5000';
-//axios.defaults.baseURL = 'https://e-shop-backend-wbt2.onrender.com';
+//axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'https://e-shop-backend-wbt2.onrender.com';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
